206042d7a4eec9de8d6fb08f60c24c23ab07bf0c
CarlosFelipeArantes <carlosfelipearantes@gmail.com>
2018-11-20 05:13:59 -0200

    Teste

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
- 
+var servidor = "https://agef.herokuapp.com/vendas?dataInicial=";
+const dateFormat = require('dateformat');
+const https = require('https');
+dataHoje = dateFormat(new Date(), "dd/mm/yyyy" );
+
+
 io.on('connection', (socket) => {
-  
-  socket.on('disconnect', function(){
-    io.emit('users-changed', {user: socket.nickname, io: 'left'});   
-  });
+
+  function getTotal(caminho){
+
+    https.get(caminho+dataHoje+"&dataFinal="+dataHoje, (res) => {
+      res.setEncoding('utf8');
+      res.on('data', function (body) {
+        io.emit('total',{total:body, nickname: socket.nickname});
+      });
+    }).on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
+  }
  
-  socket.on('set-nickname', (nickname) => {
+  socket.on('nickname', (nickname) => {
     socket.nickname = nickname;
-    io.emit('users-changed', {user: nickname, event: 'joined'});    
+  });
+
+  socket.on('total', (message) => {
+
+    getTotal(servidor);
+
+    //io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
   });
   
-  socket.on('add-message', (message) => {
-    io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+  socket.on('venda', (message) => {
+
+    if (socket.nickname.toString() == "loja1"){
+      
+    }else if( socket.nickname.toString() == "loja2" ){
+    }else{
+
+    }
+    io.emit('message', {venda: message, nickname: socket.nickname});    
   });
 });
  
@@ -23,3 +49,4 @@ var port = process.env.PORT || 3001;
 http.listen(port, function(){
    console.log('listening in http://localhost:' + port);
 });
+
