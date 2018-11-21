0281afc1eb06089c81cf9df4114c30e8a98d933d
Eric Ribeiro <ericribeiro@outlook.com.br>
2018-11-21 14:12:30 -0200

    Correção de bugs

--- index.js
+++ index.js
@@ -1,48 +1,28 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
-const dateFormat = require('dateformat');
-const https = require('https');
 
+const DATE_FORMAT = require('dateformat');
 
 io.on('connection', (socket) => {
-
-  function getTotal(caminho){
-
-    dataHoje = dateFormat(new Date(), "dd/mm/yyyy" );
-    https.get("https://agef.herokuapp.com/vendas?dataInicial="+dataHoje+"&dataFinal="+dataHoje, (res) => {
-      res.setEncoding('utf8');
-      res.on('data', function (body) {
-        //io.emit('total',{total:body, nickname: socket.nickname}); AQUI AGORA VOCE PEGA OS JSONS DE CADA
-      });
-    }).on("error", (err) => {
-      console.log("Error: " + err.message);
-    });
-  }
- 
-  socket.on('nome', (nickname) => {
-    socket.nickname = nickname;
-  });
-
-  socket.on('total', (message) => {
-    //io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
-  });
-
   socket.on('entrei', (message) => {
-    io.emit('cliente', {url: message, nickname:socket.nickname});    
+    let nickname = formatarNomeLoja(message);
+    io.emit('cliente', { url: message, nickname: nickname });
   });
-  
+
   socket.on('vendi', (message) => {
-
-    io.emit('venda', { venda: message, nickname: socket.nickname, data : dateFormat(new Date(), "dd/mm/yyyy") });
-
+    let nickname = formatarNomeLoja(message.baseUrl);
+    io.emit('venda', { venda: message, nickname: nickname, data: DATE_FORMAT(new Date(), "dd/mm/yyyy") });
   });
 
 });
- 
+
 var port = process.env.PORT || 3001;
- 
-http.listen(port, function(){
-   console.log('listening in http://localhost:' + port);
+
+http.listen(port, function () {
+  console.log('listening in http://localhost:' + port);
 });
 
+function formatarNomeLoja(baseUrl) {
+  return baseUrl.split('/')[2].split('.')[0];;
+}
